7b4d8f2155e3c6b24a4dfba1f6d978cb6100a635
romanver <romanver777@yandex.ru>
2020-02-29 16:27:06 +0300

    добавлено разделение на модули и закольцовка экранов

---
+++ js/screens/result-success.js
@@ -0,0 +1,21 @@
+import {getElemFromTemplate, renderElement} from '../utils';
+import welcomeScreen from './wellcome';
+
+const resultSuccessTemplate =
+    `<section class="main main--result">
+        <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
+        <h2 class="title">Вы настоящий меломан!</h2>
+        <div class="main-stat">За&nbsp;3&nbsp;минуты и 25&nbsp;секунд
+            <br>вы&nbsp;набрали 12 баллов (8 быстрых)
+            <br>совершив 3 ошибки
+        </div>
+        <span class="main-comparison">Вы заняли 2 место из 10. Это&nbsp;лучше чем у&nbsp;80%&nbsp;игроков</span>
+        <span role="button" tabindex="0" class="main-replay">Сыграть ещё раз</span>
+    </section>`;
+
+const resultSuccessScreen = getElemFromTemplate(resultSuccessTemplate);
+const replayButton = resultSuccessScreen.querySelector('.main-replay');
+
+replayButton.addEventListener('click', () => renderElement(welcomeScreen));
+
+export default resultSuccessScreen;
