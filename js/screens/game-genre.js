cf75b364eb3cf8434113e7b7aee8545dab6eb0fa
romanver <romanver777@yandex.ru>
2020-03-03 18:27:54 +0300

    добавлено отображение информации в модулях из данных

--- js/screens/game-genre.js
+++ js/screens/game-genre.js
@@ -1,112 +1,88 @@
 import {getElemFromTemplate, renderElement} from '../utils';
 import resultSuccessScreen from './result-success';
+import failTimeScreen from './fail-time';
 import failTriesScreen from './fail-tries';
+import {initialState, level} from '../data/data';
 
-
-const gameGenreTemplate =
-    `<section class="main main--level main--level-genre">
-    <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
+const timeline =
+    `<svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
       <circle
         cx="390" cy="390" r="370"
         class="timer-line"
         style="filter: url(../#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
+    </svg>`;
 
-      <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
+const timer =
+    `<div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
         <span class="timer-value-mins">05</span><!--
         --><span class="timer-value-dots">:</span><!--
         --><span class="timer-value-secs">00</span>
-      </div>
-    </svg>
+    </div>`;
+
+const mistake = `<img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">`;
+
+const gameGenreTemplate = (state) =>
+
+    `<section class="main main--level main--level-genre">
+
+        ${timer}     
+        ${timeline} 
+        
     <div class="main-mistakes">
-      <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
-      <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
-      <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
+        ${new Array(initialState.mistakes)
+            .fill(mistake)
+        .join('')}
     </div>
 
     <div class="main-wrap">
-      <h2 class="title">Выберите инди-рок треки</h2>
+      <h2 class="title">${state.title}</h2>
       <form class="genre">
-        <div class="genre-answer">
-          <div class="player-wrapper">
-            <div class="player">
-              <audio></audio>
-              <button class="player-control player-control--pause"></button>
-              <div class="player-track">
-                <span class="player-status"></span>
-              </div>
-            </div>
-          </div>
-          <input type="checkbox" name="answer" value="answer-1" id="a-1">
-          <label class="genre-answer-check" for="a-1"></label>
-        </div>
+        
+        ${ state.answers.map( (answer) =>
 
-        <div class="genre-answer">
-          <div class="player-wrapper">
-            <div class="player">
-              <audio></audio>
-              <button class="player-control player-control--play"></button>
-              <div class="player-track">
-                <span class="player-status"></span>
-              </div>
-            </div>
-          </div>
-          <input type="checkbox" name="answer" value="answer-1" id="a-2">
-          <label class="genre-answer-check" for="a-2"></label>
-        </div>
-
-        <div class="genre-answer">
-          <div class="player-wrapper">
-            <div class="player">
-              <audio></audio>
-              <button class="player-control player-control--play"></button>
-              <div class="player-track">
-                <span class="player-status"></span>
-              </div>
-            </div>
-          </div>
-          <input type="checkbox" name="answer" value="answer-1" id="a-3">
-          <label class="genre-answer-check" for="a-3"></label>
-        </div>
-
-        <div class="genre-answer">
-          <div class="player-wrapper">
-            <div class="player">
-              <audio></audio>
-              <button class="player-control player-control--play"></button>
-              <div class="player-track">
-                <span class="player-status"></span>
-              </div>
-            </div>
-          </div>
-          <input type="checkbox" name="answer" value="answer-1" id="a-4">
-          <label class="genre-answer-check" for="a-4"></label>
-        </div>
-
-        <button class="genre-answer-send" type="submit">Ответить</button>
+            `<div class="genre-answer">
+                <div class="player-wrapper">
+                    <div class="player">
+                        <audio></audio>
+                        <button class="player-control player-control--play"></button>
+                        <div class="player-track">
+                            <span class="player-status"></span>
+                        </div>
+                    </div>
+                </div>
+                <input type="checkbox" name="answer" value="answer-${answer.id}" id="a-${answer.id}">
+                <label class="genre-answer-check" for="a-${answer.id}"></label>
+            </div>`
+        ).join('')}
+        
+        <button class="genre-answer-send" type="submit" disabled>Ответить</button>
       </form>
     </div>
   </section>`;
 
-const gameGenreScreen = getElemFromTemplate(gameGenreTemplate);
+const gameGenreScreen = getElemFromTemplate(gameGenreTemplate( level.genre ));
 const form = gameGenreScreen.querySelector('.genre');
 const answerButton = gameGenreScreen.querySelector('.genre-answer-send');
 const answerList = gameGenreScreen.querySelectorAll('input[name=answer]');
 
-answerButton.disabled = true;
-
-answerButton.addEventListener('click', () => {
+form.addEventListener('submit', () => {
 
     let number = Math.floor(Math.random() * 11);
 
     form.reset();
     answerButton.disabled = true;
 
-    if(number > 5) {
+    if(number < 4) {
 
         renderElement(resultSuccessScreen);
-    } else {
+    }
+    else if(number > 3 && number < 8) {
 
         renderElement(failTriesScreen);
+    }
+    else {
+
+        renderElement(failTimeScreen);
     }
 });
 
--- js/screens/result-success.js
+++ js/screens/result-success.js
@@ -1,19 +1,17 @@
 import {getElemFromTemplate, renderElement} from '../utils';
 import welcomeScreen from './wellcome';
+import {level} from '../data/data';
 
-const resultSuccessTemplate =
+const resultSuccessTemplate = (state) =>
     `<section class="main main--result">
         <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
-        <h2 class="title">Вы настоящий меломан!</h2>
-        <div class="main-stat">За&nbsp;3&nbsp;минуты и 25&nbsp;секунд
-            <br>вы&nbsp;набрали 12 баллов (8 быстрых)
-            <br>совершив 3 ошибки
-        </div>
-        <span class="main-comparison">Вы заняли 2 место из 10. Это&nbsp;лучше чем у&nbsp;80%&nbsp;игроков</span>
+        <h2 class="title">${state.title}</h2>
+        <div class="main-stat">${state.stat}</div>
+        <span class="main-comparison">${state.comparison}</span>
         <span role="button" tabindex="0" class="main-replay">Сыграть ещё раз</span>
     </section>`;
 
-const resultSuccessScreen = getElemFromTemplate(resultSuccessTemplate);
+const resultSuccessScreen = getElemFromTemplate(resultSuccessTemplate( level.result.success));
 const replayButton = resultSuccessScreen.querySelector('.main-replay');
 
 replayButton.addEventListener('click', () => renderElement(welcomeScreen));
